e0662d27ff443ace9d6d15bf73f3fdba82be8607
Ray Labbe <61295936+rlabbeptc@users.noreply.github.com>
2022-04-05 17:26:58 -0400

    Setup Helper Function Folder
    Add Logging helpers

---
+++ _Helper Functions/README.md
@@ -0,0 +1,3 @@
+# Helper Functions
+
+Snippets that can be used to provide helpful functionality to Universal Device Driver profiles.
---
+++ _Helper Functions/logging.js
@@ -0,0 +1,142 @@
+/*****************************************************************************
+ * 
+ * This file is copyright (c) PTC, Inc.
+ * All rights reserved.
+ * 
+ * Name: logging.js
+ * Description: Helper Functions that can be used to add a tag for a client to control
+ * logging output to the Kepware Event log
+ * 
+ * User can create a tag with the "LoggingLevel" address to expose the current level
+ * and manipulate the logging as the driver is operating.
+ * 
+ * Enumerations:
+ * Standard Logging - 0
+ * Verbose Logging - 1
+ * Debug Logging - 2
+ * 
+ * Output will include levels below the current value i.e. Setting to Debug will include 
+ * verbose output.
+ * 
+ * Version: 0.0.1
+******************************************************************************/
+
+/**
+ * Logging Level System tag - control logging level from client application
+ * This can be used to avoid logging verbose SDS protocol messages unless 
+ * needed for debugging
+ */
+
+const LOGGING_LEVEL_TAG = {
+    address: "LoggingLevel",
+    dataType: "word",
+    readOnly: false,
+}
+const STD_LOGGING = 0;
+const VERBOSE_LOGGING = 1;
+const DEBUG_LOGGING = 2;
+// Sets initial Logging Level
+const LOGGING_LEVEL = STD_LOGGING;
+
+/** Captures the global log function so that it can be wrapped **/
+let originalLogFunction = log;
+log = function (msg, level = STD_LOGGING) {
+    switch (readFromCache(LOGGING_LEVEL_TAG.address).value) {
+        case VERBOSE_LOGGING:
+            if (level <= VERBOSE_LOGGING) {
+                originalLogFunction(msg);
+            }
+            break;
+        case DEBUG_LOGGING:
+            if (level <= DEBUG_LOGGING) {
+                originalLogFunction(msg);
+            }
+            break;
+        default:
+            if (level == STD_LOGGING) {
+                originalLogFunction(msg);
+            } 
+            break;
+    }
+}
+
+/**
+ * Below are snippets that can be added to the event handler functions for UDD
+ */
+
+function onProfileLoad() {
+    // Initialized our internal cache
+    try {
+        initializeCache();
+        
+    } catch (e){
+        // If this fails it means the cache has already been initialized
+    }
+
+    // Initialize LoggingLevel control
+    writeToCache(LOGGING_LEVEL_TAG.address, LOGGING_LEVEL);
+
+    return { version: VERSION, mode: MODE };
+}
+
+function onTagsRequest(info) {
+    // Check if tag is LoggingLevel, update from cached value
+    if (info.tags[0].address === LOGGING_LEVEL_TAG.address){
+        let returnAction = updateLoggingTag(info);
+        return returnAction;
+    }
+}
+
+
+/**
+ * Helper Functions for Logging Tag functionality 
+ */
+function onValidateTag(info) {
+
+    // Check if it's LoggingLevel tag
+    if (info.tag.address === LOGGING_LEVEL_TAG.address) {
+        info.tag = validateLoggingTag(info.tag)
+        log('onValidateTag - address "' + info.tag.address + '" is valid.', DEBUG_LOGGING)
+        return info.tag;
+    }
+
+}
+
+
+/**
+ * Validate LoggingLevel tag
+ * @param {Tag} tag 
+ * @returns {Tag} LoggingLevel Tag validation results
+ */
+
+ function validateLoggingTag(tag) {
+    if (tag.dataType === "Default"){
+        tag.dataType = "word"
+    }
+    tag.readOnly = false;
+    tag.valid = true;
+
+    return tag
+}
+
+/**
+ * Update the Logging tag to either read the value or modify the level.
+ * @param {info} info 
+ * @returns {OnTransactionResult} Transaction Result for LoggingLevel Tag
+ */
+function updateLoggingTag(info) {
+    let value = undefined;
+    if (info.type === "Write"){
+        writeToCache(LOGGING_LEVEL_TAG.address, info.tags[0].value)
+        return {action: ACTIONCOMPLETE}
+    }
+    else {
+        value = readFromCache(LOGGING_LEVEL_TAG.address).value
+        info.tags[0].value = value;
+        return { action: ACTIONCOMPLETE, tags: info.tags};
+    }
+}
+
+
+
+
